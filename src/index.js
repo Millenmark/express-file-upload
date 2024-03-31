d453e610c111dd3dec39c0f8c3bcf5d08fd7ea88
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-03-31 16:00:11 +0800

    set up index app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import "express-async-errors";
+import connectToDB from "./config/connectToDB.js";
+import notFound from "./middleware/notFound.js";
+import errorHandler from "./middleware/errorHandler.js";
+import productRoute from "./routes/productRoute.js";
 
 dotenv.config();
+connectToDB();
 const app = express();
 app.use(express.json());
+
+app.use("/api", productRoute);
+
+/** POST MIDDLEWARE */
+app.use(notFound);
+app.use(errorHandler);
 
 /** START APP */
 const port = process.env.PORT || 5000;
