6fd3ab623954cd82a0585bbdfe0e7d941fc21ff6
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-03-31 20:37:54 +0800

    added cloudinary

--- src/index.js
+++ src/index.js
@@ -5,11 +5,26 @@ import connectToDB from "./config/connectToDB.js";
 import notFound from "./middleware/notFound.js";
 import errorHandler from "./middleware/errorHandler.js";
 import productRoute from "./routes/productRoute.js";
+import fileUpload from "express-fileupload";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 connectToDB();
 const app = express();
+app.use("/uploads", express.static(join(__dirname, "public/uploads")));
 app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.use("/api", productRoute);
 
